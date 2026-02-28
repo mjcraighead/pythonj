a3d9e8fb22d0700a004d20feaa1829f5f9b7d564
Matt Craighead <mjcraighead@gmail.com>
2026-02-28 09:29:47 -0600

    Implement dict | dict

--- runtime/PyDict.java
+++ runtime/PyDict.java
@@ -189,6 +189,17 @@ public final class PyDict extends PyObject {
         }
     }
 
+    @Override public PyObject or(PyObject rhs) {
+        if (rhs instanceof PyDict rhsDict) {
+            var ret = new PyDict();
+            ret.items.putAll(items);
+            ret.items.putAll(rhsDict.items);
+            return ret;
+        } else {
+            return super.or(rhs);
+        }
+    }
+
     @Override public final boolean hasIter() { return true; }
     @Override public PyDictIter iter() { return new PyDictIter(items.keySet().iterator()); }
     @Override public PyBuiltinClass type() { return Runtime.pyglobal_dict; }
