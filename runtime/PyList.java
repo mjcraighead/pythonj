6f79ee0c42474073c9fcf56f4fb2265abe4aa5cd
Matt Craighead <mjcraighead@gmail.com>
2026-03-22 21:55:29 -0500

    Fix TypeError signature in PyList.mulInPlace()

--- runtime/PyList.java
+++ runtime/PyList.java
@@ -204,6 +204,9 @@ public final class PyList extends PyObject {
         return ret;
     }
     @Override public PyList mulInPlace(PyObject rhs) {
+        if (!rhs.hasIndex()) {
+            throw PyTypeError.raise("can't multiply sequence by non-int of type " + PyString.reprOf(rhs.type().name()));
+        }
         long count = rhs.indexValue();
         if (count <= 0) {
             items.clear();
