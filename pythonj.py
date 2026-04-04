9212617707d52fd1e18e0ac226d6e8f86a84d794
Matt Craighead <mjcraighead@gmail.com>
2026-04-04 06:11:46 -0500

    Build IR nodes for PyMethodDescriptors

--- pythonj.py
+++ pythonj.py
@@ -1884,50 +1884,46 @@ def gen_runtime_java(spec_path: str, java_path: str) -> None:
             writer.write(f'final class {java_name}Type extends PyConcreteType {{')
             writer.write(f'public static final {java_name}Type singleton = new {java_name}Type();')
             for (k, v) in attrs.items():
+                doc_value = v.get('doc')
                 if v['kind'] == 'string':
                     value = ir.CreateObject('PyString', [ir.StrLiteral(v['value'])])
-                    writer.write(f"private static final PyString pyattr_{k} = {value.emit_java(pool)};")
                 elif v['kind'] == 'member':
-                    doc_value = v.get('doc')
                     value = ir.CreateObject('PyMemberDescriptor', [
                         ir.Identifier('singleton'),
                         ir.StrLiteral(k),
                         ir.MethodRef(java_name, f'pymember_{k}'),
                         ir.Null() if doc_value is None else ir.StrLiteral(doc_value),
                     ])
-                    writer.write(f"private static final PyMemberDescriptor pyattr_{k} = {value.emit_java(pool)};")
                 elif v['kind'] == 'getset':
-                    doc_value = v.get('doc')
                     value = ir.CreateObject('PyGetSetDescriptor', [
                         ir.Identifier('singleton'),
                         ir.StrLiteral(k),
                         ir.MethodRef(java_name, f'pygetset_{k}'),
                         ir.Null() if doc_value is None else ir.StrLiteral(doc_value),
                     ])
-                    writer.write(f"private static final PyGetSetDescriptor pyattr_{k} = {value.emit_java(pool)};")
                 elif v['kind'] == 'method':
-                    doc_value = v.get('doc')
-                    doc = ir.Null().emit_java(pool) if doc_value is None else ir.StrLiteral(doc_value).emit_java(pool)
-                    constructor = ir.MethodRef(f'{java_name}Method_{k}', 'new').emit_java(pool)
-                    writer.write(f"private static final PyMethodDescriptor pyattr_{k} = new PyMethodDescriptor(singleton, {ir.StrLiteral(k).emit_java(pool)}, {constructor}, {doc});")
+                    value = ir.CreateObject('PyMethodDescriptor', [
+                        ir.Identifier('singleton'),
+                        ir.StrLiteral(k),
+                        ir.MethodRef(f'{java_name}Method_{k}', 'new'),
+                        ir.Null() if doc_value is None else ir.StrLiteral(doc_value),
+                    ])
                 elif v['kind'] == 'classmethod':
-                    doc_value = v.get('doc')
                     value = ir.CreateObject('PyClassMethodDescriptor', [
                         ir.Identifier('singleton'),
                         ir.StrLiteral(k),
                         ir.MethodRef(f'{java_name}ClassMethod_{k}', 'new'),
                         ir.Null() if doc_value is None else ir.StrLiteral(doc_value),
                     ])
-                    writer.write(f"private static final PyClassMethodDescriptor pyattr_{k} = {value.emit_java(pool)};")
                 elif v['kind'] == 'staticmethod':
                     value = ir.CreateObject('PyStaticMethod', [
                         ir.Identifier('singleton'),
                         ir.StrLiteral(k),
                         ir.CreateObject(f'{java_name}StaticMethod_{k}', [ir.Identifier('singleton')]),
                     ])
-                    writer.write(f'private static final PyStaticMethod pyattr_{k} = {value.emit_java(pool)};')
                 else:
                     assert False, (name, k, v)
+                writer.write(f'private static final {value.type} pyattr_{k} = {value.emit_java(pool)};')
             writer.write('private static final class AttrsHolder {')
             writer.write(f'static final java.util.LinkedHashMap<PyObject, PyObject> attrs = new java.util.LinkedHashMap<>({len(attrs)});')
             writer.write('static {')
